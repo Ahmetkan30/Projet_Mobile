e3fd84277995d9c686c2507d9156b72e7c792ad7
Ahmetkan <ahmetkan-18@hotmail.fr>
2021-05-16 16:27:43 +0200

    Phase2

--- app/src/main/java/com/example/projet_mobile/FragmentConnexion.java
+++ app/src/main/java/com/example/projet_mobile/FragmentConnexion.java
@@ -9,9 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class FragmentConnexion extends Fragment {
 
+    String email;
+    String mdp;
 
     public FragmentConnexion() {
 
@@ -28,10 +32,19 @@ public class FragmentConnexion extends Fragment {
         Button boutonConnection = view.findViewById(R.id.bouton_connexion);
         MainActivity activity = (MainActivity) getActivity();
         boutonConnection.setOnClickListener(v -> {
+
+            EditText e_mail = view.findViewById(R.id.mail);
+            EditText e_mot_de_passe = view.findViewById(R.id.mot_de_passe);
+            email = e_mail.getText().toString();
+            mdp = e_mot_de_passe.getText().toString();
             //Intent espacePatientIntent = new Intent(activity.getApplicationContext(), EspacePatient.class);
             //startActivity(espacePatientIntent);
+
+
             Intent espaceMedecinIntent = new Intent(activity.getApplicationContext(), Medecin.class);
             startActivity(espaceMedecinIntent);
+
+
         });
     }
 
--- app/src/main/java/com/example/projet_mobile/FragmentEnregistrer.java
+++ app/src/main/java/com/example/projet_mobile/FragmentEnregistrer.java
@@ -1,5 +1,6 @@
 package com.example.projet_mobile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,18 +8,47 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class FragmentEnregistrer extends Fragment {
 
+    String noms;
+    String prenoms;
+    String email;
+    String confi_mdp;
+    String mdp;
 
     public FragmentEnregistrer() {
     }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_fragmentenregistrer, container, false);
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        Button boutonEnregistrer = view.findViewById(R.id.bouton_enregistrer);
+        MainActivity activity = (MainActivity) getActivity();
+        boutonEnregistrer.setOnClickListener(v -> {
+
+            EditText e_noms = view.findViewById(R.id.noms);
+            EditText e_prenoms = view.findViewById(R.id.prenoms);
+            EditText e_email = view.findViewById(R.id.mail);
+            EditText e_confi_mdp = view.findViewById(R.id.mot_de_passe);
+            EditText e_mdp = view.findViewById(R.id.remot_de_passe);
+
+            noms = e_noms.getText().toString();
+            prenoms = e_prenoms.getText().toString();
+            email = e_email.getText().toString();
+            mdp = e_mdp.getText().toString();
+            confi_mdp = e_confi_mdp.getText().toString();
+
+            //Intent espacePatientIntent = new Intent(activity.getApplicationContext(), EspacePatient.class);
+            //startActivity(espacePatientIntent);
+            Intent espaceMedecinIntent = new Intent(activity.getApplicationContext(), Medecin.class);
+            startActivity(espaceMedecinIntent);
+
+
+        });
     }
 
 }
